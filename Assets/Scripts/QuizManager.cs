5080ecda3ef34ec00e39ff50ced797f11b1eb6d9
Charlie Chen <charliecaresnot@gmail.com>
2025-08-18 00:36:06 +0200

    feat: parse response json to quiz ui

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,17 +15,76 @@ namespace LegalAliens
         [SerializeField] private Transform _tfSpawnParent;
         [SerializeField] private TMP_Text _txtQuestion;
 
+        [Header("Debug")]
+        [TextArea(4, 30)] public string DebugJson;
+
+        private QuizData _currentQuizData;
+        private int _currentQuizIndex;
+
+        [ContextMenu("Prase Debug Json")]
+        private void ParseDebugJson()
+        {
+            ParseQuizDataJson(DebugJson);
+        }
+
+        private QuizData ParseQuizDataJson(string json)
+        {
+            _currentQuizData = JsonConvert.DeserializeObject<QuizData>(json);
+            return _currentQuizData;
+        }
+
+        [ContextMenu("Start Quiz")]
+        private void StartQuiz()
+        {
+            _currentQuizIndex = 0;
+            ShowQuizOnPanel(_currentQuizData.Quizzes[_currentQuizIndex]);
+        }
+
+        private void ShowQuizOnPanel(Quiz quiz)
+        {
+            SetQuestionText(quiz.Question);
+            CreateOptions(quiz.Options);
+        }
+
         private void SetQuestionText(string text)
         {
             _txtQuestion.text = text;
+        }
+
+        private void CreateOptions(string[] options)
+        {
+            foreach (Transform child in _tfSpawnParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            foreach (string option in options)
+            {
+                CreateOptionButton(option);
+            }
         }
 
         private Button CreateOptionButton(string text)
         {
             Button btn = Instantiate(_btnAnswerOptionPrefab, _tfSpawnParent);
             btn.name = "Btn_Option: " + text;
-            btn.GetComponentInChildren<Text>().text = text;
+            btn.GetComponentInChildren<TMP_Text>().text = text;
             return btn;
         }
-    } 
+    }
+
+    [Serializable]
+    public class QuizData
+    {
+        public string QuizzedObjectName;
+        public Quiz[] Quizzes;
+    }
+
+    [Serializable]
+    public class Quiz
+    {
+        public string Question;
+        public string[] Options;
+        public string Answer;
+    }
 }
