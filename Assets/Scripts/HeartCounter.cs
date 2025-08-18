6160ca5b49c2c53dc5307174dc4aea8f6bd6b8df
Charlie Chen <charliecaresnot@gmail.com>
2025-08-18 03:07:33 +0200

    feat: add sound effect at game over

--- Assets/Scripts/HeartCounter.cs
+++ Assets/Scripts/HeartCounter.cs
@@ -14,10 +14,16 @@ namespace LegalAliens
 
         [Header("Object References")]
         [SerializeField] private List<Image> _heartImages;
+        [SerializeField] private SoundManager _soundManager;
 
         public int HeartCount => _heartImages.Count;
         public event Action<int> OnHeartCountChanged;
         public event Action OnLoseAllHeats;
+
+        private void Awake()
+        {
+            if (!_soundManager) _soundManager = FindAnyObjectByType<SoundManager>();
+        }
 
         private void Start()
         {
@@ -27,12 +33,19 @@ namespace LegalAliens
         [ContextMenu("Initialize Hearts")]
         public void LoseOneHeart()
         {
+            if (_heartImages.Count == 0)
+            {
+                Debug.LogWarning("There is no heart left. How could this happen???");
+                return;
+            }
+
             Destroy(_heartImages[0].gameObject);
             _heartImages.RemoveAt(0);
             OnHeartCountChanged?.Invoke(HeartCount);
             if (HeartCount == 0)
             {
                 OnLoseAllHeats?.Invoke();
+                _soundManager.PlayGameOverSound();
                 // TODO: VFX SFX etc.
                 Debug.Log("YOU DIED!!!!");
             }
